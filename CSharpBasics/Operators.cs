c5ced46d57097a2fc4214a7e2f0c1c59c0084291
Yangji <yangjihyolmo@Yangjis-MacBook-Air.local>
2026-03-10 19:47:40 +0545

    Feat: adds assignment operators example

--- CSharpBasics/Operators.cs
+++ CSharpBasics/Operators.cs
@@ -9,7 +9,14 @@ namespace OperatorsBasics
             //arithemetic operators
             int x = 10;
             int y = 20;
-            Console.WriteLine("The sum of number is "+ (x+y));
+            Console.WriteLine("The sum of number is " + (x + y));
+
+            //assignment operators
+            int x = 10;
+            x += 5;
+            Console.WriteLine(x);
+
+            
         }
     }
 }
