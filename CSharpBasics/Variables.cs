655d8a5521bbffd6b6e7259c094620ffff2f735a
Yangji <yangjihyolmo@Yangjis-MacBook-Air.local>
2026-03-10 19:20:06 +0545

    Feat: add Variables.cs file

---
+++ CSharpBasics/Variables.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace VariableBasics
+{
+    public class Variable
+    {
+        public void VariableExample()
+        {
+            string name = "\nJohn";
+            Console.WriteLine(name);
+
+            int age = 19;
+            Console.WriteLine(age);
+
+            double myDoubleNum = 5.99D;
+            Console.WriteLine(myDoubleNum);
+
+            char myLetter = 'D';
+            Console.WriteLine(myLetter);
+
+            bool myBool = true;
+            Console.WriteLine(myBool+"\n");
+        }
+
+    }
+}
