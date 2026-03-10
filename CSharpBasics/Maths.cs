55405c6731a284054706b3ebd7af6baabd2f7a97
Yangji <yangjihyolmo@Yangjis-MacBook-Air.local>
2026-03-10 20:05:22 +0545

    Feat: add Math class examples for max, min, sqrt, abs, and round

--- CSharpBasics/Maths.cs
+++ CSharpBasics/Maths.cs
@@ -6,7 +6,11 @@ namespace MathsBasics
     {
         public void MathsExample()
         {
-                
+            Console.WriteLine("The max number is " + Math.Max(5, 10));  //to find out max number
+            Console.WriteLine("The min number is " + Math.Min(5, 10));  //to find out min number
+            Console.WriteLine(Math.Sqrt(25));  //to find out square number
+            Console.WriteLine(Math.Abs(-4.10));  //to find out absolute number
+            Console.WriteLine(Math.Round(1.99)); //to find out nearest whole number after rounding
         }
     }
 }
