9309955b720828ec4ef877f6a84be6306b3aa386
Yangji <yangjihyolmo@Yangjis-MacBook-Air.local>
2026-03-10 19:35:30 +0545

    Feat : adds age input and display in console

--- CSharpBasics/UserInput.cs
+++ CSharpBasics/UserInput.cs
@@ -5,13 +5,19 @@ namespace UserInputBasics
     public class UserInput
     {
         public void UserInputExample()
-    {
-        Console.WriteLine("Enter UserName: ");
+        {
+            Console.WriteLine("Enter UserName: ");
 
-        String UserName = Console.ReadLine();
+            String UserName = Console.ReadLine();
 
-        Console.WriteLine("UserName is " + UserName);
+            Console.WriteLine("UserName is " + UserName);
 
-    }
+            Console.WriteLine("\nEnter your age:");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: " + age);
+
+        }
     }
 }
